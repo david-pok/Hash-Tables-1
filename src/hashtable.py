276017d1191eb7fb87fc9e54121f1fdc978e7bb1
david-pok <david.pok@gmail.com>
2019-10-29 18:38:05 -0700

    finishes mvp

--- src/hashtable.py
+++ src/hashtable.py
@@ -32,6 +32,12 @@ class HashTable:
 
         OPTIONAL STRETCH: Research and implement DJB2
         '''
+    # # Start from an arbitrary large prime
+    # hash_value = 5381
+    # # Bit-shift and sum value for each character
+    # for char in key:
+    #     hash_value = ((hash_value << 5) + hash_value) + char
+    # return hash_value
         pass
 
 
@@ -66,7 +72,7 @@ class HashTable:
 
         if node is None:
             inserting.next = self.storage[index]
-            self.storage[index] = node
+            self.storage[index] = inserting
         else:
             node.value = value
 
@@ -94,11 +100,14 @@ class HashTable:
             prev_node = node
             node = prev_node.next
 
-        if node.key == key:
-            prev_node.next = node.next
-
         if node is None:
             print("Not Found")
+
+        else:
+            if prev_node is None:
+                self.storage[index] = node.next
+            else:
+                prev_node.next = node.next
 
     def retrieve(self, key):
         '''
@@ -121,12 +130,12 @@ class HashTable:
         if node is None:
             return None
 
-        while node is not None and node.key != key:
+        while node is not None:
+            if node.key == key:
+                return node.value
+
             next_node = node.next
             node = next_node
-
-        if node.key == key:
-            return node.value
 
     def resize(self):
         '''
@@ -135,9 +144,19 @@ class HashTable:
 
         Fill this in.
         '''
-        pass
+        #save copy of old storage in variable
+        #double capacity/storage
+        #iterate through and copy old storage into new storage
+        #point storage to new storage
+        old_capacity = self.capacity
+        old_storage = self.storage
+        self.capacity *= 2
+        self.storage = [None] * self.capacity
 
-
+        for x in old_storage:
+            while x is not None:
+                self.insert(x.key, x.value)
+                x = x.next
 
 if __name__ == "__main__":
     ht = HashTable(2)
